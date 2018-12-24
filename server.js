fa683269ecf86748b7df5888c3718b0b1c3d5617
seen2 <tiwarietc@gmail.com>
2018-12-24 12:40:57 +0530

    implemented server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express=require('express');
+
+const app=express();
+
+app.get('/',(req,res)=>{
+	res.send('hello');
+});
+
+const port=5000;// || process.env.PORT 
+
+app.listen(port,()=>console.log(`listening on port ${port}...`));
