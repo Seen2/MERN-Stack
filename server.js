43afec11402f56d538f7c490b3972283e974313c
seen2 <tiwarietc@gmail.com>
2018-12-26 10:30:33 +0530

    done with basic setups

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
-const express=require('express');
+const express=require('express'),
+	bodyParser=require('body-parser'),
+	mongoose=require('mongoose'),
+	{URI}=require('./config/keys');
+
+const users=require('./routes/api/users');
+const posts=require('./routes/api/posts');
+const profile=require('./routes/api/profile');
+
+//console.log(JSON.stringify(URI));
+
+
+mongoose.connect(URI, { useNewUrlParser: true })
+
 
 const app=express();
 
 app.get('/',(req,res)=>{
 	res.send('hello');
 });
+
+app.use('/api/users',users);
+app.use('/api/posts',posts);
+app.use('/api/profile',profile);
 
 const port=5000;// || process.env.PORT 
 
