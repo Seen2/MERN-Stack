df0a22a4dbcc8128217570c3515d55aa2e98e772
seen2 <tiwarietc@gmail.com>
2018-12-31 00:49:19 +0530

    desginig login form

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,66 @@
 import React, { Component } from "react";
 
 export default class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      password: "",
+      email: "",
+      errors: {}
+    };
+  }
+  onInputChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+  onSubmit = event => {
+    event.preventDefault();
+
+    const { email, password } = this.state;
+    const currentUser = { email, password };
+    console.log(currentUser);
+  };
+
   render() {
     return (
       <div>
-        <h1>Login</h1>
+        <div className="login">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-8 m-auto">
+                <h1 className="display-4 text-center">Log In</h1>
+                <p className="lead text-center">
+                  Sign in to your DevConnector account
+                </p>
+                <form onSubmit={this.onSubmit}>
+                  <div className="form-group">
+                    <input
+                      type="email"
+                      className="form-control form-control-lg"
+                      placeholder="Email Address"
+                      name="email"
+                      value={this.state.email}
+                      onChange={this.onInputChange}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="password"
+                      className="form-control form-control-lg"
+                      placeholder="Password"
+                      name="password"
+                      value={this.state.password}
+                      onChange={this.onInputChange}
+                    />
+                  </div>
+                  <input
+                    type="submit"
+                    className="btn btn-info btn-block mt-4"
+                  />
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
