0b20c130fc863d3bb21cee3e3ef3134d8cbdaee3
seen2 <tiwarietc@gmail.com>
2019-01-01 19:18:52 +0530

    created a spinner component

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,27 +1,63 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 import { getCurrentProfile } from "../../actions/profileActions";
+import Spinner from "../common/Spinner";
 
 export class Dashboard extends Component {
   static propTypes = {
-    prop: PropTypes
+    getCurrentProfile: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    profile: PropTypes.object.isRequired
   };
   componentDidMount() {
     this.props.getCurrentProfile();
   }
 
   render() {
+    const { user } = this.props.auth;
+    const { profile, loading } = this.props.profile;
+    let dashboardContent;
+    if (profile === null || loading) {
+      dashboardContent = <Spinner />;
+    } else {
+      //check if login user has profile data
+      if (Object.keys(profile).length > 0) {
+        dashboardContent = <h4>TODO Display Profile</h4>;
+      } else {
+        //user logged in but has no profile
+        dashboardContent = (
+          <div>
+            <p className="lead text-muted">Welcome {user.name}</p>
+            <p>You have not setup your profile please enter some info</p>
+            <Link to="/create-proifle" className="btn btn-lg btn-info">
+              Create Profile
+            </Link>
+          </div>
+        );
+      }
+    }
     return (
-      <div>
-        <h1>Dashboard</h1>
+      <div className="dashboard">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="diplay-4">Dashboard</h1>
+              {dashboardContent}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-const mapStateToProps = state => ({});
+const mapStateToProps = state => ({
+  profile: state.profile,
+  auth: state.auth
+});
 
 const mapDispatchToProps = {
   getCurrentProfile
