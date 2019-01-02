13e2250f86cbab0b2a15a0c3566d1f09baa63350
seen2 <tiwarietc@gmail.com>
2019-01-02 09:47:06 +0530

    bug fixed

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -1,17 +1,16 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
-
+import PropTypes from "prop-types";
 import TextFieldGroup from "../common/TextFieldGroup";
 import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
-import SelectListGroup from "../common/SelectListGroup";
 import InputGroup from "../common/InputGroup";
+import SelectListGroup from "../common/SelectListGroup";
 import { createProfile } from "../../actions/profileActions";
 
-export class CreateProfile extends Component {
-  constructor() {
-    super();
+class CreateProfile extends Component {
+  constructor(props) {
+    super(props);
     this.state = {
       displaySocialInputs: false,
       handle: "",
@@ -29,14 +28,20 @@ export class CreateProfile extends Component {
       instagram: "",
       errors: {}
     };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
+
   componentWillReceiveProps(nextProps) {
     if (nextProps.errors) {
       this.setState({ errors: nextProps.errors });
     }
   }
-  onSubmit = event => {
-    event.preventDefault();
+
+  onSubmit(e) {
+    e.preventDefault();
+
     const profileData = {
       handle: this.state.handle,
       company: this.state.company,
@@ -52,15 +57,19 @@ export class CreateProfile extends Component {
       youtube: this.state.youtube,
       instagram: this.state.instagram
     };
+
     this.props.createProfile(profileData, this.props.history);
-  };
-  onChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
 
   render() {
     const { errors, displaySocialInputs } = this.state;
+
     let socialInputs;
+
     if (displaySocialInputs) {
       socialInputs = (
         <div>
@@ -111,9 +120,10 @@ export class CreateProfile extends Component {
         </div>
       );
     }
-    // select options for status
+
+    // Select options for status
     const options = [
-      { label: "* SELECT PROFFESIONAL STATUS", value: 0 },
+      { label: "* Select Professional Status", value: 0 },
       { label: "Developer", value: "Developer" },
       { label: "Junior Developer", value: "Junior Developer" },
       { label: "Senior Developer", value: "Senior Developer" },
@@ -123,6 +133,7 @@ export class CreateProfile extends Component {
       { label: "Intern", value: "Intern" },
       { label: "Other", value: "Other" }
     ];
+
     return (
       <div className="create-profile">
         <div className="container">
@@ -130,9 +141,9 @@ export class CreateProfile extends Component {
             <div className="col-md-8 m-auto">
               <h1 className="display-4 text-center">Create Your Profile</h1>
               <p className="lead text-center">
-                Let's get some information to create your profile stand out
+                Let's get some information to make your profile stand out
               </p>
-              <small className="d-block pb-3"> required feilds</small>
+              <small className="d-block pb-3">* = required fields</small>
               <form onSubmit={this.onSubmit}>
                 <TextFieldGroup
                   placeholder="* Profile Handle"
@@ -140,16 +151,16 @@ export class CreateProfile extends Component {
                   value={this.state.handle}
                   onChange={this.onChange}
                   error={errors.handle}
-                  info="A unique name for your profile URL."
+                  info="A unique handle for your profile URL. Your full name, company name, nickname"
                 />
                 <SelectListGroup
-                  options={options}
                   placeholder="Status"
-                  name="staus"
+                  name="status"
                   value={this.state.status}
                   onChange={this.onChange}
+                  options={options}
                   error={errors.status}
-                  info="Give us a idea of what you are in your carrier"
+                  info="Give us an idea of where you are at in your career"
                 />
                 <TextFieldGroup
                   placeholder="Company"
@@ -157,7 +168,7 @@ export class CreateProfile extends Component {
                   value={this.state.company}
                   onChange={this.onChange}
                   error={errors.company}
-                  info="Could be your own or one you work for"
+                  info="Could be your own company or one you work for"
                 />
                 <TextFieldGroup
                   placeholder="Website"
@@ -182,7 +193,7 @@ export class CreateProfile extends Component {
                   onChange={this.onChange}
                   error={errors.skills}
                   info="Please use comma separated values (eg.
-                    HTML,CSS,JavaScript,PHP)"
+                    HTML,CSS,JavaScript,PHP"
                 />
                 <TextFieldGroup
                   placeholder="Github Username"
@@ -200,6 +211,7 @@ export class CreateProfile extends Component {
                   error={errors.bio}
                   info="Tell us a little about yourself"
                 />
+
                 <div className="mb-3">
                   <button
                     type="button"
@@ -213,13 +225,13 @@ export class CreateProfile extends Component {
                     Add Social Network Links
                   </button>
                   <span className="text-muted">Optional</span>
-                  {socialInputs}
-                  <input
-                    type="submit"
-                    value="Submit"
-                    className="btn btn-info btn-block mt-4"
-                  />
                 </div>
+                {socialInputs}
+                <input
+                  type="submit"
+                  value="Submit"
+                  className="btn btn-info btn-block mt-4"
+                />
               </form>
             </div>
           </div>
@@ -231,13 +243,12 @@ export class CreateProfile extends Component {
 
 CreateProfile.propTypes = {
   profile: PropTypes.object.isRequired,
-  errors: PropTypes.object
+  errors: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
-  createProfile: PropTypes.func.isRequired,
   profile: state.profile,
-  errors: state.errors.isRequired
+  errors: state.errors
 });
 
 export default connect(
